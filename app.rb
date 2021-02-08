cb781477732893b808ccb76f858fa9295740e7c1
Sydney Daniels <sydeast@gmail.com>
2021-02-08 17:48:28 -0600

    commit tree cb10b41bada7b9298c2533848d9497fc293419ab

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! This is some added text. Adding more text to see if Shotgun works without stopping the server."
   end
 
 end
